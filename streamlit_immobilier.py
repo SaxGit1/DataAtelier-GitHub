009ec8a47b8ec98f471e7820ace36a183956cb05
SaxGit1 <158860513+SaxGit1@users.noreply.github.com>
2024-02-05 01:51:44 +0100

    Update streamlit_immobilier.py

--- streamlit_immobilier.py
+++ streamlit_immobilier.py
@@ -4,12 +4,14 @@ Created on Tue Jul 18 14:57:06 2023
 
 @author: PC.054
 """
-!pip install seaborn
+
+
 
 import pandas as pd 
 import numpy as np 
 import streamlit as st 
 import seaborn as sns 
+python -m pip install seaborn
 import matplotlib.pyplot as plt
 import plotly.express as px
 from sklearn.model_selection import train_test_split
